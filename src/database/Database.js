5a34085484ae52f01584c1e7450eaf058ddf8f34
pedro-drosa <pedro_drosa@hotmail.com>
2023-03-28 11:55:03 -0300

    feat: persist data in a json file

--- src/database/Database.js
+++ src/database/Database.js
@@ -1,5 +1,18 @@
+import { readFile, writeFile } from "node:fs/promises";
+const databasePath = new URL("../../db.json", import.meta.url);
+
 export class Database {
   #tables = {};
+
+  constructor() {
+    readFile(databasePath, "utf8")
+      .then((data) => (this.#tables = JSON.parse(data)))
+      .catch(() => this.#persist());
+  }
+
+  #persist() {
+    writeFile(databasePath, JSON.stringify(this.#tables));
+  }
 
   insert(table, data) {
     if (Array.isArray(this.#tables[table])) {
@@ -7,6 +20,7 @@ export class Database {
     } else {
       this.#tables[table] = [data];
     }
+    this.#persist();
     return data;
   }
 
@@ -24,11 +38,17 @@ export class Database {
 
   delete(table, id) {
     const rowIndex = this.#tables[table].findIndex((row) => row.id === id);
-    if (rowIndex > -1) this.#tables[table].splice(rowIndex, 1);
+    if (rowIndex > -1) {
+      this.#tables[table].splice(rowIndex, 1);
+      this.#persist();
+    }
   }
 
   update(table, id, data) {
     const rowIndex = this.#tables[table].findIndex((row) => row.id === id);
-    this.#tables[table][rowIndex] = data;
+    if (rowIndex > -1) {
+      this.#tables[table][rowIndex] = data;
+      this.#persist();
+    }
   }
 }
